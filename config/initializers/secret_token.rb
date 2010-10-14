52a8220bf10a567844df7c74b1faf8560f5d4809
Winston Eddie-Edirin <eddwinston@gmail.com>
2010-10-15 00:27:11 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afrojamsnite::Application.config.secret_token = 'd2cef1b41d17a308c19ebf672668b35c1edfa50c33320776f1f9ae60973676bb68c7a65c7e47d5fe94e98fc2eac198de145fe2859b4ea4e9a3bd069bd3765db9'
