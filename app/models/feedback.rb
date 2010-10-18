e97d6f72c85a0a54450247e35f7ccd90ab30678c
Winston Eddie-Edirin <eddwinston@gmail.com>
2010-10-18 07:55:23 +0300

    Image positioning

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,15 @@
 class Feedback
+  
   attr_accessor :name, :email, :message
+
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @email = attributes[:email]
+    @message = attributes[:message]
+  end
+  
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+  
 end
